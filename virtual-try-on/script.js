9bb6f8495f19294e32e9ce1d22c0e92aaa7295cd
bietbaogiomoigiau333-cell <bietbaogiomoigiau333@gmail.com>
2025-12-27 22:09:02 +0700

    Add files via upload

---
+++ virtual-try-on/script.js
@@ -0,0 +1,11 @@
+function changeAvatar(path) {
+  document.getElementById("avatar").src = path;
+}
+
+function changeTop(path) {
+  document.getElementById("top").src = path;
+}
+
+function changeBottom(path) {
+  document.getElementById("bottom").src = path;
+}
